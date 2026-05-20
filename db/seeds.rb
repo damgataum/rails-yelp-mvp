ee8cd6466e7ae01233cd88a4584e96c7cab480db
Daniel dos Santos <ddsantos@gmx.de>
2026-05-20 15:24:40 +0200

    implement seeds with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "faker"
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+10.times do
+  new_restaurant = Restaurant.new
+  new_restaurant.name = Faker::Restaurant.name
+  new_restaurant.address = Faker::Address.street_address
+  new_restaurant.phone_number = Faker::PhoneNumber.phone_number
+  new_restaurant.category = [ "chinese", "italian", "japanese", "french", "belgian" ][rand(5)]
+  new_restaurant.save
+  rand(6).times do
+      new_review = Review.new(content: Faker::Restaurant.review, rating: rand(6))
+      new_review.restaurant = new_restaurant
+      new_review.save
+  end
+end
+
+puts "Done!"
